418ff4addd29b5b915f6a4a7cb59675a6ac3e999
Dan <danmil511@gmail.com>
2025-03-30 20:06:08 -0500

    Fixed navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,40 +4,41 @@ import { LogIn } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div className="h-25 bg-black flex items-center">
-      <div className="ml-13">
+    <div className="relative h-20 bg-black flex items-center justify-between px-6">
+      {/* Logo - Left */}
+      <div className="flex items-center">
         <img
           src={logo}
-          alt=""
-          className="h-30 transition-transform duration-[2000ms] hover:rotate-[180deg]"
+          alt="Logo"
+          className="h-25 transition-transform duration-[2000ms] hover:rotate-[180deg]"
         />
       </div>
-      <div className="space-x-8 m-auto">
+
+      {/* Center Links */}
+      <div className="absolute left-1/2 -translate-x-1/2 flex space-x-8">
         <a
           href="#"
-          className="text-white hover:text-amber-400 transition transform ease-in-out duration-300"
+          className="text-white hover:text-amber-400 transition duration-300"
         >
           Recommended
         </a>
         <a
           href="#"
-          className="text-white hover:text-amber-400 transition transform ease-in-out duration-300"
+          className="text-white hover:text-amber-400 transition duration-300"
         >
           Favorites
         </a>
         <a
           href="#"
-          className="text-white hover:text-amber-400 transition transform ease-in-out duration-300"
+          className="text-white hover:text-amber-400 transition duration-300"
         >
           Search
         </a>
       </div>
-      <div className="mr-15">
-        <LogIn
-          size={30}
-          color="orange"
-          className="w-6 h-6 hover:scale-110 transition-transform duration-200"
-        />
+
+      {/* Login Icon - Right */}
+      <div className="text-white hover:scale-110 transition-transform duration-200">
+        <LogIn size={26} color="orange" />
       </div>
     </div>
   );
