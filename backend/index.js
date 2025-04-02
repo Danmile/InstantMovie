b1dfdc6e988d954787a3b4f5e13741049dd05e35
Dan <danmil511@gmail.com>
2025-04-02 12:57:46 -0500

    Added cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import moviesRoutes from "./routes/movie.route.js";
-
+import cors from "cors";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
