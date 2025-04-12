02c53ba6b24cc96ed7cf1142ef4d08a084ed5e6c
Dan <danmil511@gmail.com>
2025-04-12 15:24:48 -0500

    applied a slight effect when loading the grid

--- frontend/src/components/MovieGrid.jsx
+++ frontend/src/components/MovieGrid.jsx
@@ -3,8 +3,18 @@ import { useMovieStore } from "../store/useMovieStore";
 import { useEffect, useState, useRef } from "react";
 
 function MovieGrid({ movies = [] }) {
+  const [showGrid, setShowGrid] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowGrid(true), 100);
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <div className="p-6">
+    <div
+      className={`p-6 transition-opacity duration-500 ${
+        showGrid ? "opacity-100" : "opacity-0"
+      }`}
+    >
       <div className="flex flex-wrap gap-8 justify-center items-center space-x-25">
         {movies.map((movie, index) => (
           <MovieCard key={index} movie={movie} />
