1643ac0c20997b46f401e39f4ce598f6813fc35d
Dan <danmil511@gmail.com>
2025-04-17 13:56:29 -0500

    Some fixes for deployment

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import moviesRoutes from "./routes/movie.route.js";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -18,6 +20,14 @@ app.use(
 app.use(express.json());
 
 app.use("/api/movies", moviesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
